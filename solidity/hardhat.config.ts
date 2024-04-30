87bcb5d4197820904adecb36928dec74fa0a1834
Ben Longstaff <ben.longstaff@thesis.co>
2024-04-30 17:31:34 +0930

    Adding deployment scripts for dummy contract
    Using existing scaffolding to deploy and verify a contract on etherscan.

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -1,10 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config"
+import "@keep-network/hardhat-helpers"
 import "@nomicfoundation/hardhat-toolbox"
 import "@nomicfoundation/hardhat-chai-matchers"
 import "@openzeppelin/hardhat-upgrades"
 import "hardhat-deploy"
 import "hardhat-contract-sizer"
 import "hardhat-gas-reporter"
+import dotenv from "dotenv-safer"
+
+dotenv.config({
+  allowEmptyValues: true,
+  example: process.env.CI ? ".env.ci.example" : ".env.example",
+})
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+  ? process.env.MAINNET_RPC_URL
+  : ""
+
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY
+  ? [process.env.MAINNET_PRIVATE_KEY]
+  : []
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+  ? process.env.SEPOLIA_RPC_URL
+  : ""
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
+  ? [process.env.SEPOLIA_PRIVATE_KEY]
+  : []
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+  ? process.env.ETHERSCAN_API_KEY
+  : ""
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -19,8 +46,46 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: "typechain",
   },
-  paths: {
-    sources: "./contracts",
+  networks: {
+    mainnet: {
+      url: MAINNET_RPC_URL,
+      accounts: MAINNET_PRIVATE_KEY,
+      chainId: 1,
+      tags: ["etherscan"],
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: SEPOLIA_PRIVATE_KEY,
+      chainId: 11155111,
+      tags: ["allowStubs", "etherscan"],
+    },
+    hardhat: {
+      tags: ["allowStubs"],
+    },
+  },
+  external: {
+    deployments: {
+      sepolia: ["./external/sepolia"],
+      mainnet: ["./external/mainnet"],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: 0,
+    governance: {
+      default: 0,
+      mainnet: "0x98d8899c3030741925be630c710a98b57f397c7a",
+    },
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    strict: true,
+  },
+  gasReporter: {
+    enabled: true,
   },
 }
 
