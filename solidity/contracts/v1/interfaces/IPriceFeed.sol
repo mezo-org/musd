83c6383204f1eb916ee8f286b7b5cab191851e8d
Beau Rancourt <beau.rancourt@proton.me>
2024-07-18 17:12:34 -0400

    Implement a simple PriceFeed
    As long as we have an interface, we can use evmos magic to populate our transactions pricefeed however we'd like in go-land.
    
    So, to cause the least amount of migration pain, we standardize on the chainlink interface since that's the most popular among blue-chips.

--- solidity/contracts/v1/interfaces/IPriceFeed.sol
+++ solidity/contracts/v1/interfaces/IPriceFeed.sol
@@ -6,6 +6,10 @@ interface IPriceFeed {
     // --- Events ---
     event LastGoodPriceUpdated(uint256 _lastGoodPrice);
 
+    event NewOracleRegistered(address _oracle);
+
     // --- Function ---
     function fetchPrice() external returns (uint);
+
+    function setOracle(address _oracle) external;
 }
