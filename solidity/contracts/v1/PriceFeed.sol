4693f75cf5a67b9c1712e942ef38e5f932012977
Beau Rancourt <beau.rancourt@proton.me>
2024-07-24 11:15:50 -0400

    Let slither know that we dont need the return values

--- solidity/contracts/v1/PriceFeed.sol
+++ solidity/contracts/v1/PriceFeed.sol
@@ -24,6 +24,7 @@ contract PriceFeed is IPriceFeed, Ownable {
             );
 
         require(chainLinkOracle.decimals() > 0, "Invalid Decimals from Oracle");
+        // slither-disable-next-line unused-return
         (, int256 price, , , ) = chainLinkOracle.latestRoundData();
         require(price != 0, "Oracle returns 0 for price");
 
@@ -35,6 +36,7 @@ contract PriceFeed is IPriceFeed, Ownable {
     // Public functions -------------------------------------------------------------------------------------------------
 
     function fetchPrice() public view virtual returns (uint256) {
+        // slither-disable-next-line unused-return
         (, int256 price, , , ) = oracle.latestRoundData();
         return _scalePriceByDigits(uint256(price), oracle.decimals());
     }
