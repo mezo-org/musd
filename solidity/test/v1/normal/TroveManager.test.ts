92e7407b70447536870ef888130636e614ec7998
Ryan Watts <rtwatts07@gmail.com>
2024-08-05 13:00:10 -0700

    Test that liquidate removes the correct trove from the TroveOwners array, and moves the last array element to the new empty slot

--- solidity/test/v1/normal/TroveManager.test.ts
+++ solidity/test/v1/normal/TroveManager.test.ts
@@ -21,6 +21,9 @@ describe("TroveManager in Normal Mode", () => {
   let addresses: TestingAddresses
   let alice: User
   let bob: User
+  let carol: User
+  let dennis: User
+  let eric: User
   let state: ContractsState
   let contracts: Contracts
   let cachedTestSetup: TestSetup
@@ -37,6 +40,9 @@ describe("TroveManager in Normal Mode", () => {
     // users
     alice = testSetup.users.alice
     bob = testSetup.users.bob
+    carol = testSetup.users.carol
+    dennis = testSetup.users.dennis
+    eric = testSetup.users.eric
 
     // readability helper
     addresses = await getAddresses(contracts, testSetup.users)
@@ -206,5 +212,71 @@ describe("TroveManager in Normal Mode", () => {
 
     state.troveManager.stakes.after = await contracts.troveManager.totalStakes()
     expect(state.troveManager.stakes.after).to.be.equal(bob.trove.stake.before)
+  })
+
+  it("liquidate(): Removes the correct trove from the TroveOwners array, and moves the last array element to the new empty slot", async () => {
+    // Open additional troves with decreasing collateral ratio
+    await openTrove(contracts, {
+      musdAmount: "5000",
+      ICR: "218",
+      sender: carol.wallet,
+    })
+    await openTrove(contracts, {
+      musdAmount: "5000",
+      ICR: "216",
+      sender: dennis.wallet,
+    })
+    await openTrove(contracts, {
+      musdAmount: "5000",
+      ICR: "214",
+      sender: eric.wallet,
+    })
+
+    /*
+     Our TroveOwners array should now be: [Alice, Bob, Carol, Dennis, Eric].
+     Note they are not sorted by ICR but by insertion order.
+    */
+    state.troveManager.troves.before =
+      await contracts.troveManager.getTroveOwnersCount()
+    expect(state.troveManager.troves.before).to.be.equal(5)
+
+    // Drop the price to lower ICRs below MCR
+    await contracts.mockAggregator.setPrice(to1e18(1000))
+
+    await contracts.troveManager.liquidate(carol.wallet.address)
+
+    // Check that carol no longer has an active trove
+    expect(await contracts.sortedTroves.contains(carol.wallet)).to.equal(false)
+
+    // Check that the TroveOwners array has been updated correctly
+    state.troveManager.troves.after =
+      await contracts.troveManager.getTroveOwnersCount()
+    expect(state.troveManager.troves.after).to.be.equal(4)
+
+    /* After Carol is removed from the array, the last element (Eric's address) should have been moved to fill the
+     * empty slot left by Carol. The TroveOwners array should now be: [Bob, Alice, Eric, Dennis] */
+    const troveOwners = await Promise.all([
+      contracts.troveManager.TroveOwners(0),
+      contracts.troveManager.TroveOwners(1),
+      contracts.troveManager.TroveOwners(2),
+      contracts.troveManager.TroveOwners(3),
+    ])
+
+    expect(troveOwners[0]).to.be.equal(addresses.alice)
+    expect(troveOwners[1]).to.be.equal(addresses.bob)
+    expect(troveOwners[2]).to.be.equal(addresses.eric)
+    expect(troveOwners[3]).to.be.equal(addresses.dennis)
+
+    // Check that the correct indices are recorded on the active trove structs
+    const troveStructs = await Promise.all([
+      contracts.troveManager.Troves(addresses.alice),
+      contracts.troveManager.Troves(addresses.bob),
+      contracts.troveManager.Troves(addresses.eric),
+      contracts.troveManager.Troves(addresses.dennis),
+    ])
+    expect(troveStructs[0][4]).to.be.equal(0)
+    expect(troveStructs[1][4]).to.be.equal(1)
+    expect(troveStructs[2][4]).to.be.equal(2)
+    expect(troveStructs[3][4]).to.be.equal(3)
   })
 })
