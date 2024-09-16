e4563ea482a8e7ed9ac5c9c42782455d1bf28d93
Ryan Watts <rtwatts07@gmail.com>
2024-09-16 11:14:09 -0700

    Use "0" as CR for edge case hint helpers test

--- solidity/test/v1/normal/HintHelpers_getApproxHint.test.ts
+++ solidity/test/v1/normal/HintHelpers_getApproxHint.test.ts
@@ -11,7 +11,9 @@ describe("HintHelpers", () => {
   let frank: User
   let contracts: Contracts
 
-  const latestRandomSeed = 31337
+  // eslint-disable-next-line prefer-const
+  let latestRandomSeed = 31337n
+  let hintAddress: string
   const sqrtLength = Math.ceil(Math.sqrt(6)) // Sqrt of the number of Troves
 
   beforeEach(async () => {
@@ -41,38 +43,41 @@ describe("HintHelpers", () => {
 
     // CR = 202% (Carol's Trove)
     const cr = to1e18("202")
-    const { hintAddress } = await contracts.hintHelpers.getApproxHint(
-      cr,
-      sqrtLength * 10,
-      latestRandomSeed,
-    )
+    ;({ hintAddress, latestRandomSeed } =
+      await contracts.hintHelpers.getApproxHint(
+        cr,
+        sqrtLength * 10,
+        latestRandomSeed,
+      ))
     const firstTrove = await contracts.sortedTroves.getFirst()
     expect(hintAddress).to.eq(firstTrove)
   })
 
-  it("getApproxHint: returns the head of the list if the CR is the max uint256 value", async () => {
+  it("getApproxHint(): returns the head of the list if the CR is the max uint256 value", async () => {
     await setupTroves()
 
     // CR = Maximum value, i.e. 2**256 -1
     const crMax =
       "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-    const { hintAddress } = await contracts.hintHelpers.getApproxHint(
-      crMax,
-      sqrtLength * 10,
-      latestRandomSeed,
-    )
+    ;({ hintAddress, latestRandomSeed } =
+      await contracts.hintHelpers.getApproxHint(
+        crMax,
+        sqrtLength * 10,
+        latestRandomSeed,
+      ))
     expect(hintAddress).to.eq(await contracts.sortedTroves.getFirst())
   })
 
   it("getApproxHint(): returns the tail of the list if the CR is lower than ICR of any Trove", async () => {
     await setupTroves()
+    ;({ hintAddress, latestRandomSeed } =
+      await contracts.hintHelpers.getApproxHint(
+        "0",
+        sqrtLength * 10,
+        latestRandomSeed,
+      ))
 
-    const crMin = to1e18("110")
-    const { hintAddress } = await contracts.hintHelpers.getApproxHint(
-      crMin,
-      sqrtLength * 10,
-      latestRandomSeed,
-    )
+    await contracts.sortedTroves.getLast()
 
     expect(hintAddress).to.eq(await contracts.sortedTroves.getLast())
   })
