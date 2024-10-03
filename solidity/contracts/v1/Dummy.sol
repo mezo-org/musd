7a2c242ce2dde59d79c5fcee21680927b94fd0a1
Ryan Watts <rtwatts07@gmail.com>
2024-10-03 10:33:06 -0700

    Remove dummy contracts

--- solidity/contracts/v1/Dummy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.24;
-
-contract Dummy {
-    function nope() external pure returns (uint256) {
-        return 0;
-    }
-}
--- solidity/contracts/v2/DummyV2.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.24;
-
-contract DummyV2 {
-    function nope() external pure returns (uint256) {
-        return 0;
-    }
-}
