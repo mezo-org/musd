ef766ad5ae09b7150aa89d09a43e51b3f11f6a54
Beau Rancourt <beau.rancourt@proton.me>
2024-10-04 11:03:44 -0400

    Deploy an unconnected price feed contract for hardhat testing

--- solidity/deploy/14_deploy_price_feed.ts
+++ solidity/deploy/14_deploy_price_feed.ts
@@ -3,7 +3,7 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { waitConfirmationsNumber } from "../helpers/deploy-helpers"
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { deployments, helpers, getNamedAccounts } = hre
+  const { deployments, helpers, getNamedAccounts, network } = hre
   const { log } = deployments
   const { deployer } = await getNamedAccounts()
 
@@ -14,6 +14,18 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     log("Deploying PriceFeed contract...")
 
     await deployments.deploy("PriceFeed", {
+      contract: "PriceFeed",
+      args: [],
+      from: deployer,
+      log: true,
+      waitConfirmations: waitConfirmationsNumber(hre),
+    })
+  }
+
+  if (network.name === "hardhat") {
+    log("Deploying Unconnected PriceFeed contract...")
+
+    await deployments.deploy("UnconnectedPriceFeed", {
       contract: "PriceFeed",
       args: [],
       from: deployer,
