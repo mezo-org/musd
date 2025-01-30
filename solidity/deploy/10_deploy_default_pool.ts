2c4c8fbd5997a7c9adaf1c4428b87fe9b35b1c66
Beau Rancourt <beau.rancourt@proton.me>
2025-01-30 10:21:25 -0500

    convert default pool to proxy

--- solidity/deploy/10_deploy_default_pool.ts
+++ solidity/deploy/10_deploy_default_pool.ts
@@ -3,8 +3,8 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { setupDeploymentBoilerplate } from "../helpers/deploy-helpers"
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { getOrDeploy } = await setupDeploymentBoilerplate(hre)
-  await getOrDeploy("DefaultPool")
+  const { getOrDeployProxy } = await setupDeploymentBoilerplate(hre)
+  await getOrDeployProxy("DefaultPool")
 }
 
 export default func
