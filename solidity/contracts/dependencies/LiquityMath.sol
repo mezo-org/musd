ac6165cce2cb9d67519471c6a63097674e437ddc
Ryan Watts <rtwatts07@gmail.com>
2025-02-07 10:28:55 -0800

    Remove unused LiquityMath functions

--- solidity/contracts/dependencies/LiquityMath.sol
+++ solidity/contracts/dependencies/LiquityMath.sol
@@ -3,8 +3,6 @@
 pragma solidity ^0.8.24;
 
 library LiquityMath {
-    uint256 internal constant DECIMAL_PRECISION = 1e18;
-
     /* Precision for Nominal ICR (independent of price). Rationale for the value:
      *
      * - Making it “too high” could lead to overflows.
@@ -18,71 +16,6 @@ library LiquityMath {
 
     function _min(uint256 _a, uint256 _b) internal pure returns (uint) {
         return (_a < _b) ? _a : _b;
-    }
-
-    /*
-     * Multiply two decimal numbers and use normal rounding rules:
-     * -round product up if 19'th mantissa digit >= 5
-     * -round product down if 19'th mantissa digit < 5
-     *
-     * Used only inside the exponentiation, _decPow().
-     */
-    function decMul(
-        uint256 x,
-        uint256 y
-    ) internal pure returns (uint256 decProd) {
-        uint256 prod_xy = x * y;
-
-        decProd = (prod_xy + (DECIMAL_PRECISION / 2)) / DECIMAL_PRECISION;
-    }
-
-    /*
-     * _decPow: Exponentiation function for 18-digit decimal base, and integer exponent n.
-     *
-     * Uses the efficient "exponentiation by squaring" algorithm. O(log(n)) complexity.
-     *
-     * Called by one function that represent time in units of minutes:
-     * 1) TroveManager._calcDecayedBaseRate
-     *
-     * The exponent is capped to avoid reverting due to overflow. The cap 525600000 equals
-     * "minutes in 1000 years": 60 * 24 * 365 * 1000
-     *
-     * If a period of > 1000 years is ever used as an exponent in either of the above functions, the result will be
-     * negligibly different from just passing the cap, since:
-     *
-     * In function 1), the decayed base rate will be 0 for 1000 years or > 1000 years
-     * In function 2), the difference in tokens issued at 1000 years and any time > 1000 years, will be negligible
-     */
-    function _decPow(
-        uint256 _base,
-        uint256 _minutes
-    ) internal pure returns (uint) {
-        if (_minutes > 5.256e8) {
-            _minutes = 5.256e8;
-        } // cap to avoid overflow
-
-        if (_minutes == 0) {
-            return DECIMAL_PRECISION;
-        }
-
-        uint256 y = DECIMAL_PRECISION;
-        uint256 x = _base;
-        uint256 n = _minutes;
-
-        // Exponentiation-by-squaring
-        while (n > 1) {
-            if (n % 2 == 0) {
-                x = decMul(x, x);
-                n = n / 2;
-            } else {
-                // if (n % 2 != 0)
-                y = decMul(x, y);
-                x = decMul(x, x);
-                n = (n - 1) / 2;
-            }
-        }
-
-        return decMul(x, y);
     }
 
     function _getAbsoluteDifference(
