8dbc9d2e4a52f83431badba1a1e7fc672e9ea7d1
Beau Rancourt <beau.rancourt@proton.me>
2025-02-14 16:20:02 -0500

    Add understores to variable names

--- solidity/contracts/dependencies/InterestRateMath.sol
+++ solidity/contracts/dependencies/InterestRateMath.sol
@@ -8,10 +8,10 @@ library InterestRateMath {
     function calculateInterestOwed(
         uint256 _principal,
         uint16 _interestRate,
-        uint256 startTime,
-        uint256 endTime
+        uint256 _startTime,
+        uint256 _endTime
     ) internal pure returns (uint256) {
-        uint256 timeElapsed = endTime - startTime;
+        uint256 timeElapsed = _endTime - _startTime;
         return
             (_principal * _interestRate * timeElapsed) /
             (10000 * SECONDS_IN_A_YEAR);
