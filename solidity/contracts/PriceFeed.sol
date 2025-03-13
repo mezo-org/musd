d0abb00fb1eb84370b239ddc5708b05bffb44e47
Beau Rancourt <beau.rancourt@proton.me>
2025-03-13 13:23:20 -0400

    Rename ONE_MINUTE_IN_SECONDS to MAX_PRICE_DELAY

--- solidity/contracts/PriceFeed.sol
+++ solidity/contracts/PriceFeed.sol
@@ -10,7 +10,8 @@ import "./interfaces/IPriceFeed.sol";
 contract PriceFeed is IPriceFeed, Ownable2StepUpgradeable {
     /// @dev Used to convert an oracle price answer to an 18-digit precision uint
     uint8 public constant TARGET_DIGITS = 18;
-    uint256 private constant ONE_MINUTE_IN_SECONDS = 60;
+    // If the oracle has not been updated in at least 60 seconds, it is stale.
+    uint256 private constant MAX_PRICE_DELAY = 60;
 
     // State ------------------------------------------------------------------------------------------------------------
     ChainlinkAggregatorV3Interface public oracle;
@@ -46,7 +47,7 @@ contract PriceFeed is IPriceFeed, Ownable2StepUpgradeable {
         // slither-disable-next-line unused-return
         (, int256 price, , uint256 updatedAt, ) = oracle.latestRoundData();
         require(
-            block.timestamp - updatedAt <= ONE_MINUTE_IN_SECONDS,
+            block.timestamp - updatedAt <= MAX_PRICE_DELAY,
             "PriceFeed: Oracle is stale."
         );
         return _scalePriceByDigits(uint256(price), oracle.decimals());
