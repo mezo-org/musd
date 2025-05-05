3d954657cb75072a5e755fc92805086482f0b3c0
Piotr Dyraga <piotr.dyraga@thesis.co>
2025-05-05 13:04:48 +0200

    Simplified upgradeability tests for PriceFeed
    For upgradeability tests, we can trust the toolkit and the virtual
    machine. The address is not going to change, and the contract will be
    usable from other contracts. What we need is a very basic test ensuring
    `helpers.upgrades.upgradeProxy` does not fail.
    
    Also, `PriceFeedUpgradeTester` was not entirely correct :) There is no
    need for a gap in that contract, and `initialize(address _owner)` was
    not called. If it was called, it would fail as the version 1 was
    already initialized (the `initializer` modifier).

--- solidity/test/upgrade/upgrade-proxy.ts
+++ solidity/test/upgrade/upgrade-proxy.ts
@@ -4,23 +4,16 @@ import { helpers } from "hardhat"
 import {
   Contracts,
   User,
-  openTrove,
   setupTests,
-  updateTroveSnapshot,
 } from "../helpers"
-import { to1e18 } from "../utils"
 
 describe("Proxy Upgrades", () => {
   let contracts: Contracts
-  let carol: User
-  let whale: User
+  let deployer: User
 
   beforeEach(async () => {
-    ;({ contracts, carol, whale } = await setupTests())
+    ;({ contracts, deployer } = await setupTests())
   })
-
-  const updatePriceFeed = async () =>
-    helpers.upgrades.upgradeProxy("PriceFeed", "PriceFeedUpgradeTester")
 
   it("upgrades InterestRateManager contract correctly", async () => {
     const interestRate = await contracts.interestRateManager.interestRate()
@@ -185,51 +178,26 @@ describe("Proxy Upgrades", () => {
     expect(await upgradedDefaultPool.newField()).to.equal(213)
   })
 
-  it("do not change the underlying address", async () => {
-    const oldPrice = await contracts.priceFeed.fetchPrice()
-    const oldAddress = await contracts.priceFeed.getAddress()
-    expect(oldPrice).to.equal(to1e18("50,000"))
+  it("upgrades PriceFeed contract correctly", async () => {
+    const [upgradedPriceFeed] = await helpers.upgrades.upgradeProxy(
+      "PriceFeed",
+      "PriceFeedV2",
+      {
+        proxyOpts: {
+          call: {
+            fn: "initializeV2",
+          },
+        },
+      },
+    )
 
-    await updatePriceFeed()
+    // state preserved and previous functionality works
+    expect(await upgradedPriceFeed.owner()).to.equal(
+      deployer.address
+    )
 
-    const newPrice = await contracts.priceFeed.fetchPrice()
-    const newAddress = await contracts.priceFeed.getAddress()
-    expect(newPrice).to.equal(to1e18("45,000"))
-    expect(newAddress).to.equal(oldAddress)
-  })
-
-  it("automatically interoperate with connected contracts", async () => {
-    await openTrove(contracts, {
-      musdAmount: "300,000",
-      ICR: "200",
-      sender: whale.wallet,
-    })
-
-    await openTrove(contracts, {
-      musdAmount: "2,000",
-      ICR: "120",
-      sender: carol.wallet,
-    })
-
-    // Updating the price feed in place reduces the BTC price from $50k to $45k,
-    // lowering carol's ICR to a liquidatable number.
-    await updatePriceFeed()
-
-    await contracts.troveManager.connect(whale.wallet).liquidate(carol.wallet)
-
-    await updateTroveSnapshot(contracts, carol, "after")
-
-    expect(carol.trove.debt.after).to.equal(0n)
-  })
-
-  it("preserves prior state", async () => {
-    const oldOracle = await contracts.priceFeed.oracle()
-
-    await updatePriceFeed()
-
-    const newOracle = await contracts.priceFeed.oracle()
-
-    expect(newOracle).to.equal(oldOracle)
-    expect(newOracle).to.equal(await contracts.mockAggregator.getAddress())
+    // new functionality works
+    await upgradedPriceFeed.newFunction()
+    expect(await upgradedPriceFeed.newField()).to.equal(398)
   })
 })
