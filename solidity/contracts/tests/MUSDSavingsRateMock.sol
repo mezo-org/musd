8815dca1faad4eb5d975578e93774c506fd4db03
Dymitr (Dmitry) Paremski <dymitr.paremski@thesis.co>
2025-11-25 00:39:33 +0100

    Slithering and linting Mock contract

--- solidity/contracts/tests/MUSDSavingsRateMock.sol
+++ solidity/contracts/tests/MUSDSavingsRateMock.sol
@@ -6,7 +6,7 @@ import "../interfaces/IMUSDSavingsRate.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MUSDSavingsRateMock is IMUSDSavingsRate {
-    IERC20 public musd;
+    IERC20 public immutable musd;
     uint256 public totalYieldReceived;
     uint256 public callCount;
 
@@ -14,22 +14,22 @@ contract MUSDSavingsRateMock is IMUSDSavingsRate {
         musd = IERC20(_musd);
     }
 
+    function reset() external {
+        totalYieldReceived = 0;
+        callCount = 0;
+    }
+
     function receiveProtocolYield(uint256 amount) external override {
+        totalYieldReceived += amount;
+        callCount += 1;
         require(
             musd.transferFrom(msg.sender, address(this), amount),
             "MUSDSavingsRateMock: transfer failed"
         );
-        totalYieldReceived += amount;
-        callCount += 1;
     }
 
     // Helper functions for testing
     function getBalance() external view returns (uint256) {
         return musd.balanceOf(address(this));
-    }
-
-    function reset() external {
-        totalYieldReceived = 0;
-        callCount = 0;
     }
 }
