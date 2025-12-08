e54d9444ed527a7cd861d3669f9a3038b4d6e72e
Dymitr (Dmitry) Paremski <dymitr.paremski@thesis.co>
2025-12-08 14:28:14 +0100

    Add docs fro IBTCYieldReceiver interface

--- solidity/contracts/interfaces/IBTCYieldReceiver.sol
+++ solidity/contracts/interfaces/IBTCYieldReceiver.sol
@@ -2,6 +2,13 @@
 
 pragma solidity 0.8.24;
 
+/// @title IBTCYieldReceiver
+/// @notice Interface for contracts that receive protocol yield in BTC
 interface IBTCYieldReceiver {
+    /// @notice Receives protocol yield in BTC
+    /// @param btcAmount The amount of BTC yield being distributed
+    /// @dev This function is called by the PCV contract after sending BTC
+    ///      to the receiver. The receiver should handle the BTC yield according
+    ///      to its implementation (e.g., converting to another asset)
     function receiveProtocolYieldInBTC(uint256 btcAmount) external;
 }
